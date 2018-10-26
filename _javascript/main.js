e33c40096a41288bf8f6a4240d42d16da0c5afbc
Perezjo94 <joey.perez13@gmail.com>
2018-10-25 23:30:09 -0400

    Hero carousel clean up

--- _javascript/main.js
+++ _javascript/main.js
@@ -38,21 +38,22 @@ document.addEventListener('DOMContentLoaded', () => {
   var insideHtml = '';
 
   for(i = 0; i < slides.length; i++ ) {
-    insideHtml += `<span id="carousel-navigation-slide-icon-${i}" class="level-item icon"><i class="${slides[i].className === 'carousel-item is-active' ? 'fas' : 'far'} fa-circle"></i></span>`;
+    insideHtml += `<span id="carousel-navigation-slide-icon-${i}" class="circle-nav level-item icon"><i class="${slides[i].className === 'carousel-item is-active' ? 'fas' : 'far'} fa-circle"></i></span>`;
   }
 
   slideDots.innerHTML = insideHtml;
 
   carousels.forEach(instance => {
     instance.on('carousel:slide:after', slide => {
-      // console.log('carousel-navigation-slide-icon-' + slide.node.dataset.slideIndex);
-      var slideEl = document.getElementById('carousel-navigation-slide-icon-' + slide.node.dataset.slideIndex);
-      slideEl.innerHTML = '<i class="fas fa-circle"></i>';
-
-      var prevSlideIndex = slide.node.dataset.slideIndex === '0' ? document.querySelectorAll('[data-slide-index]').length - 1 : slide.node.dataset.slideIndex - 1;
-      var prevSlideEl = document.getElementById('carousel-navigation-slide-icon-' + prevSlideIndex);
-
-      prevSlideEl.innerHTML = '<i class="far fa-circle"></i>';
+      var slideDotEls = document.querySelectorAll('.circle-nav');
+      slideDotEls.forEach((slideEl, index) => {
+        if(index == slide.node.dataset.slideIndex) {
+          slideEl.innerHTML = '<i class="fas fa-circle"></i>';
+        }
+        else {
+          slideEl.innerHTML = '<i class="far fa-circle"></i>';
+        }
+      });
     });
   });
 
