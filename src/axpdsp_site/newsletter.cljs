37634387c33edbb5c3013edf154750315a483553
Reily Siegel <mail@reilysiegel.com>
2020-07-31 23:19:17 -0400

    Improve the handling of long newsletters
    Show newsletters full-width on all devices, and hide all newsletters initially.

--- src/axpdsp_site/newsletter.cljs
+++ src/axpdsp_site/newsletter.cljs
@@ -7,39 +7,40 @@
 
 
 (defn ui []
-  (let [entry-index (r/atom 0)]
+  (let [entry-index (r/atom nil)]
     (fn []
       [:section.section.is-small.has-background-light {:id "newsletter"}
        [:div.container
+        [:div.content
+         [:h1 "Newsletter - Fresh off the Press"]]
         [:div.columns
-         [:div.column.is-6
-          [:div.content
-           [:h1 "Newsletter - Fresh off the Press"]]
-          [:div.columns
-           (for [[{:keys [title author position date]}
-                  idx] (map vector (take 6 newsletter) (range))]
-             [:div.column.is-6
-              {:key idx}
-              [:div.card
-               {:style {:height         "100%"
-                        :display        :flex
-                        :flex-direction :column}}
-               [:div.card-content
-                {:style {:height "100%"}}
-                [:div.media-content
-                 [:p.title title]
-                 [:p.subtitle (.format (js/Intl.DateTimeFormat.
-                                        "en-US"
-                                        #js {:day   "numeric"
-                                             :month "long"
-                                             :year  "numeric"})
-                                       date)]
-                 [:p.subtitle (str author " - " position)]]]
-               [:div.card-footer
-                [:a.card-footer-item
-                 {:style   {:margin-top :auto}
-                  :onClick #(reset! entry-index idx)}
-                 "View"]]]])]]
-         [:div.column.is-offset-1.is-5
-          [:div.content {:dangerouslySetInnerHTML
-                         {:__html (:html (nth newsletter @entry-index))}}]]]]])))
+         (for [[{:keys [title author position date]}
+                idx] (map vector (take 4 newsletter) (range))]
+           [:div.column.is-3
+            {:key idx}
+            [:div.card
+             {:style {:height         "100%"
+                      :display        :flex
+                      :flex-direction :column}}
+             [:div.card-content
+              {:style {:height "100%"}}
+              [:div.media-content
+               [:p.title title]
+               [:p.subtitle (.format (js/Intl.DateTimeFormat.
+                                      "en-US"
+                                      #js {:day   "numeric"
+                                           :month "long"
+                                           :year  "numeric"})
+                                     date)]
+               [:p.subtitle (apply str author
+                                   (when position
+                                     [" - " position]))]]]
+             [:div.card-footer
+              [:a.card-footer-item
+               {:style   {:margin-top :auto}
+                :onClick #(reset! entry-index idx)}
+               "View"]]]])]
+        (when @entry-index
+          [:div.content.is-medium
+           {:dangerouslySetInnerHTML
+            {:__html (:html (nth newsletter @entry-index))}}])]])))
