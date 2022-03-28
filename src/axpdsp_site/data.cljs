09a213efd9f695c11d673d6976afd375bd5a7b03
Reily Siegel <mail@reilysiegel.com>
2022-03-28 13:31:23 +0200

    Ensure latest updated brother wins for brothers with conflicting positions.

--- src/axpdsp_site/data.cljs
+++ src/axpdsp_site/data.cljs
@@ -18,25 +18,38 @@
   ((set positions) position))
 
 (defn with-positions [positions brothers]
-
   (into
    []
    (comp (dedupe)
          (keep (fn [position]
-                 (first (filter (partial has-position? position) brothers)))))
+                 (first (filter (partial has-position? position)
+                                (sort-by :timestamp (comp - compare) brothers))))))
    positions))
+
+(defn true-positions
+  ([brother] (true-positions @brothers brother))
+  ([brothers {:keys [positions scroll]
+              :as   brother}]
+   (let [positions (zipmap positions (with-positions positions brothers))]
+     (assoc brother :positions (into []
+                                     (comp
+                                      (filter (comp #{scroll} :scroll val))
+                                      (map key))
+                                     positions)))))
 
 (a/go (let [brothers-res (sheets/sheet->map
                           "1m1Ne5-iynuYu9IOXeBHtIcrv6vEjjgTrYlXsBqf02ko"
                           {:key-fn sheets.key-fns/idiomatic-keyword
-                           :page   "Website"})]
-        (reset! brothers
-                (shuffle
-                 (into []
-                       (comp
-                        (map #(update % :positions sheets-list->set))
-                        (map #(update % :major sheets-list->set)))
-                       (a/<! brothers-res))))))
+                           :page   "Website"})
+            brothers-res (shuffle
+                          (into []
+                                (comp
+                                 (map #(update % :positions sheets-list->set))
+                                 (map #(update % :major sheets-list->set)))
+                                (a/<! brothers-res)))]
+        (reset! brothers (into []
+                               (map (partial true-positions brothers-res))
+                               brothers-res))))
 
 (a/go (let [rush-res (sheets/sheet->map
                       "1El7AF_OReo2qZqSrskB6DAAkN-v6EHchO-pP8ch1xD8"
